e6b110f834ec7d30b2921574a3316739bae652c2
Samuel Huang <samhuang91@gmail.com>
2014-10-07 13:11:34 -0700

    Remove private OSX files

--- uninstall.sh
+++ uninstall.sh
@@ -11,13 +11,20 @@ echo "Stopping boot2docker processes..."
 boot2docker stop && boot2docker delete
 
 echo "Removing boot2docker executable..."
-sudo rm /usr/local/bin/boot2docker
+sudo rm -f /usr/local/bin/boot2docker
 
 echo "Removing boot2docker ISO and socket files..."
 rm -rf ~/.boot2docker
 rm -rf /usr/local/share/boot2docker
 
+echo "Removing boot2docker SSH keys..."
+sudo rm -f ~/.ssh/id_boot2docker*
+
+echo "Removing boot2docker OSX files..."
+sudo rm -f /private/var/db/receipts/io.boot2docker.*
+sudo rm -f /private/var/db/receipts/io.boot2dockeriso.*
+
 echo "Removing Docker executable..."
-sudo rm /usr/local/bin/docker
+sudo rm -f /usr/local/bin/docker
 
 echo "All Done!"
