7893a69328c5bc560c2007b215c84b0e33c80203
Samuel Huang <samhuang91@gmail.com>
2014-10-08 08:03:17 -0700

    Fix uninstall script for bash 4.3.29

--- uninstall.sh
+++ uninstall.sh
@@ -2,29 +2,34 @@
 
 # Uninstall Script
 
-if [ -n "$(which boot2docker)" ] ; then
+if [ "$(which boot2docker)" == "" ]; then
     echo "boot2docker does not exist on your machine!"
     exit 1
+fi
+
+if [ "${USER}" != "root" ]; then
+    echo "$0 must be run as root!"
+    exit 2
 fi
 
 echo "Stopping boot2docker processes..."
 boot2docker stop && boot2docker delete
 
 echo "Removing boot2docker executable..."
-sudo rm -f /usr/local/bin/boot2docker
+rm -f /usr/local/bin/boot2docker
 
 echo "Removing boot2docker ISO and socket files..."
 rm -rf ~/.boot2docker
 rm -rf /usr/local/share/boot2docker
 
 echo "Removing boot2docker SSH keys..."
-sudo rm -f ~/.ssh/id_boot2docker*
+rm -f ~/.ssh/id_boot2docker*
 
 echo "Removing boot2docker OSX files..."
-sudo rm -f /private/var/db/receipts/io.boot2docker.*
-sudo rm -f /private/var/db/receipts/io.boot2dockeriso.*
+rm -f /private/var/db/receipts/io.boot2docker.*
+rm -f /private/var/db/receipts/io.boot2dockeriso.*
 
 echo "Removing Docker executable..."
-sudo rm -f /usr/local/bin/docker
+rm -f /usr/local/bin/docker
 
 echo "All Done!"
