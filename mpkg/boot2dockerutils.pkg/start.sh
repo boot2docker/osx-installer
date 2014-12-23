484e3e425d866df778a1b08e2d7a547f4c59a005
Julien Bordellier <julien.bordellier@etixgroup.com>
2014-12-23 13:41:10 +0100

    Using the ideas behind the @artiom version

--- mpkg/boot2dockerutils.pkg/start.sh
+++ mpkg/boot2dockerutils.pkg/start.sh
@@ -6,5 +6,5 @@ mkdir -p ~/.boot2docker
 if [ ! -f ~/.boot2docker/boot2docker.iso ]; then cp /usr/local/share/boot2docker/boot2docker.iso ~/.boot2docker/ ; fi
 $b2d ip
 $b2d init
-$b2d up && export DOCKER_HOST=tcp://$(/usr/local/bin/boot2docker ip 2>/dev/null):2375
+$b2d up && export DOCKER_HOST=tcp://$(/usr/local/bin/boot2docker ip 2>/dev/null):2376
 docker version
